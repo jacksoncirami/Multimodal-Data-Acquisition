0c610249641f6b576f4b7ae11901e043b7533461
Jackson Cirami <jackson.cirami@gmail.com>
2026-07-07 17:10:40 -0400

    Create bertecsdk_lsl_test.cs

---
+++ lsl_streaming/bertecsdk_lsl_test.cs
@@ -0,0 +1,39 @@
+using System;
+using System.Threading;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Starting fake LSL test stream...");
+        Console.WriteLine("Open LabRecorder and look for: FakeForceTest");
+        Console.WriteLine("Press Ctrl+C to stop.\n");
+
+        liblsl.StreamInfo info = new liblsl.StreamInfo(
+            "FakeForceTest",
+            "Force",
+            1,
+            100.0,
+            liblsl.channel_format_t.cf_float32,
+            "fake_force_test_001"
+        );
+
+        liblsl.StreamOutlet outlet = new liblsl.StreamOutlet(info);
+
+        float[] sample = new float[1];
+        int counter = 0;
+
+        while (true)
+        {
+            sample[0] = counter;
+
+            outlet.push_sample(sample);
+
+            Console.WriteLine("Sent sample: " + sample[0]);
+
+            counter++;
+
+            Thread.Sleep(10);
+        }
+    }
+}
